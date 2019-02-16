c9c5759d9c9cb15d48d6fd9e6a23dc4f97cb67c8
L D <tapircode@gmail.com>
2019-02-16 23:59:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(artists, instruments){
+  var list = []
+  for (var i = 0; i < artists.length; i++){
+    var str = `${artists[i]} plays ${instruments[i]}`
+    list.push(str)
+  }
+}
